a84f9ec32a684497a4fc2efa3c2c14183af4e5a1
Your Name <amine.triki15@gmail.com>
2025-06-24 17:12:45 +0100

    editing  nav bar

--- frontEnd/src/components/Nav.tsx
+++ frontEnd/src/components/Nav.tsx
@@ -1,4 +1,5 @@
-import  { useState, type JSX, type MouseEvent } from "react";
+import React from "react";
+import { useState, type JSX, type MouseEvent } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -10,149 +11,187 @@ import Container from "@mui/material/Container";
 import MenuItem from "@mui/material/MenuItem";
 import { NavLink } from "react-router-dom";
 import AmineLogo from "../assets/4-157-71-68080953ce2d3.webp";
+import AccountCircle from "@mui/icons-material/AccountCircle";
 
-const pages: string[] = ["Home", "Projects", "E-commerce" ,"Skills", "Contact"];
+const pages: string[] = ["Home", "Projects", "E-commerce", "Skills", "Contact"];
 
 function Nav(): JSX.Element {
-    const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    const handleOpenNavMenu = (event: MouseEvent<HTMLElement>): void => {
-        setAnchorElNav(event.currentTarget);
-    };
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
 
-    const handleCloseNavMenu = (): void => {
-        setAnchorElNav(null);
-    };
+  const handleOpenNavMenu = (event: MouseEvent<HTMLElement>): void => {
+    setAnchorElNav(event.currentTarget);
+  };
 
-    return (
-        <AppBar
-            position="static"
+  const handleCloseNavMenu = (): void => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        background: "linear-gradient(to right,#16B4F2, #ffffff)",
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h4"
+            noWrap
+            component="a"
+            href="/"
             sx={{
-                background: "linear-gradient(to right,#16B4F2, #ffffff)",
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontWeight: 700,
+              letterSpacing: ".2rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
-        >
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <Typography
-                        variant="h4"
-                        noWrap
-                        component="a"
-                        href="/"
-                        sx={{
-                            mr: 2,
-                            display: { xs: "none", md: "flex" },
-                            fontWeight: 700,
-                            letterSpacing: ".2rem",
-                            color: "inherit",
-                            textDecoration: "none",
-                        }}
-                    >
-                        Amine Triki
-                    </Typography>
+          >
+            Amine Triki
+          </Typography>
 
-                    <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: "bottom",
-                                horizontal: "left",
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: "top",
-                                horizontal: "left",
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{ display: { xs: "block", md: "none" } }}
-                        >
-                            {pages.map((page) => (
-                                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                                    <NavLink
-                                        to={
-                                            page.toLowerCase() === "home"
-                                                ? "/"
-                                                : `/${page.toLowerCase()}`
-                                        }
-                                        style={({ isActive }: { isActive: boolean }) => ({
-                                            textDecoration: "none",
-                                            color: isActive ? "#F2A413" : "black",
-                                            width: "100%",
-                                            textAlign: "center",
-                                            padding: "8px 0",
-                                            fontWeight: 600,
-                                        })}
-                                    >
-                                        {page}
-                                    </NavLink>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: "block", md: "none" } }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <NavLink
+                    to={
+                      page.toLowerCase() === "home"
+                        ? "/"
+                        : `/${page.toLowerCase()}`
+                    }
+                    style={({ isActive }: { isActive: boolean }) => ({
+                      textDecoration: "none",
+                      color: isActive ? "#F2A413" : "black",
+                      width: "100%",
+                      textAlign: "center",
+                      padding: "8px 0",
+                      fontWeight: 600,
+                    })}
+                  >
+                    {page}
+                  </NavLink>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
 
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href="#app-bar-with-responsive-menu"
-                        sx={{
-                            mr: 2,
-                            display: { xs: "flex", md: "none" },
-                            flexGrow: 1,
-                            fontWeight: 700,
-                            letterSpacing: ".2rem",
-                            color: "inherit",
-                            textDecoration: "none",
-                        }}
-                    >
-                        Amine Triki
-                    </Typography>
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: "none", md: "flex" },
-                            justifyContent: "center",
-                        }}
-                    >
-                        {pages.map((page) => (
-                            <NavLink
-                                key={page}
-                                to={
-                                    page.toLowerCase() === "home"
-                                        ? "/"
-                                        : `/${page.toLowerCase()}`
-                                }
-                                style={({ isActive }: { isActive: boolean }) => ({
-                                    textDecoration: "none",
-                                    color: isActive ? "#F2A413" : "white",
-                                    margin: "0 10px",
-                                    padding: "10px 15px",
-                                    fontWeight: 700,
-                                })}
-                                onClick={handleCloseNavMenu}
-                            >
-                                {page}
-                            </NavLink>
-                        ))}
-                    </Box>
-                    <Box sx={{ flexGrow: 0 }}>
-                        <img src={AmineLogo} alt="Amine Logo" style={{ height: "40px" }} />
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar>
-    );
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontWeight: 700,
+              letterSpacing: ".2rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Amine Triki
+          </Typography>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              justifyContent: "center",
+            }}
+          >
+            {pages.map((page) => (
+              <NavLink
+                key={page}
+                to={
+                  page.toLowerCase() === "home" ? "/" : `/${page.toLowerCase()}`
+                }
+                style={({ isActive }: { isActive: boolean }) => ({
+                  textDecoration: "none",
+                  color: isActive ? "#F2A413" : "white",
+                  margin: "0 10px",
+                  padding: "10px 15px",
+                  fontWeight: 700,
+                })}
+                onClick={handleCloseNavMenu}
+              >
+                {page}
+              </NavLink>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <img src={AmineLogo} alt="Amine Logo" style={{ height: "40px" }} />
+          </Box>
+
+          <Box sx={{ margin: '0 10px' }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
 
 export default Nav;
