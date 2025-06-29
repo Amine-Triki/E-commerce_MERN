32211a04c5450697825306bd7f5f48cbc8713634
Your Name <amine.triki15@gmail.com>
2025-06-29 12:27:35 +0100

    cart page layout

--- frontEnd/src/pages/CartPage/CartPage.tsx
+++ frontEnd/src/pages/CartPage/CartPage.tsx
@@ -1,24 +1,55 @@
 import { Box, Typography } from "@mui/material";
 import Container from "@mui/material/Container";
-import { useEffect, useState } from "react";
-import { baseUrl } from "../../constants/baseUrl";
-import { useAuth } from "../../context/Auth/AuthContext";
 import { useCart } from "../../context/Cart/CartContext";
-
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 const CartPage = () => {
-  const { token } = useAuth();
   const { cartItems, totalAmount } = useCart();
-  const [error, setError] = useState("");
-
-
 
   return (
     <main>
-      <Container sx={{ mt: 2 }}>
+      <Container fixed sx={{ mt: 2 }}>
         <Typography variant="h4">my cart</Typography>
-        {cartItems.map((item) => (
-          <Box>{item.title}</Box>
-        ))}
+        <Box display="flex" flexDirection="column" gap={4}>
+          {cartItems.map((item) => (
+            <Box
+              display="flex"
+              flexDirection="row"
+              justifyContent="space-between"
+              alignItems="center"
+              sx={{
+                border: 1,
+                borderColor: "#f2f2f2",
+                borderRadius: 5,
+                padding: 1,
+              }}
+            >
+              <Box
+                display="flex"
+                flexDirection="row"
+                alignItems="center"
+                gap={1}
+              >
+                <img src={item.image} width={50} />
+                <Box>
+                  <Typography variant="h6">{item.title}</Typography>
+                  <Typography>
+                    {item.quantity} x {item.unitPrice} TND{" "}
+                  </Typography>
+                </Box>
+              </Box>
+              <ButtonGroup variant="contained" aria-label="Basic button group">
+                <Button>-</Button>
+                <Button>+</Button>
+              </ButtonGroup>
+            </Box>
+          ))}
+          <Box>
+            <Typography variant="h4">
+              Total Amount: {totalAmount.toFixed(2)} TNDs
+            </Typography>
+          </Box>
+        </Box>
       </Container>
     </main>
   );
